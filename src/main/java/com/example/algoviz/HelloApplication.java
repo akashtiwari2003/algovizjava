8b1e3bf546320e0ac7be899ddc5850a8a81a77a4
Sahu-Abhi <abhisahu162001@gmail.com>
2022-11-11 02:45:08 +0530

    0.1v

--- src/main/java/com/example/algoviz/HelloApplication.java
+++ src/main/java/com/example/algoviz/HelloApplication.java
@@ -14,17 +14,16 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
 
-        Image img = new Image("Algoviz_Logo.png");
+//        Setting Title and Icon
+        stage.setTitle("Algo Viz");
         Image icon = new Image("Algoviz_Logo.png");
         stage.getIcons().add(icon);
-        stage.setTitle("Algo Viz");
 
 
-//
-        ImageView logo = new ImageView(img);
+
+
+        stage.setScene(scene);
         stage.show();
     }
 
