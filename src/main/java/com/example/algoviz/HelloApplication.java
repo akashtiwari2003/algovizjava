9b88dfd013791955c7ecc73fcca2e9c969e2d78b
Sahu-Abhi <abhisahu162001@gmail.com>
2022-11-11 02:42:15 +0530

    0v

--- src/main/java/com/example/algoviz/HelloApplication.java
+++ src/main/java/com/example/algoviz/HelloApplication.java
@@ -1,7 +1,9 @@
 package com.example.algoviz;
 
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,23 +12,23 @@ import javafx.stage.Stage;
 import java.io.IOException;
 
 public class HelloApplication extends Application {
+
+    @FXML
+    ImageView logo;
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        FXMLLoader root = FXMLLoader.load(getClass().getResource("hello-view.fxml"));
+        Scene scene = new Scene(root.load());
 
-        Image img = new Image("Algoviz_Logo.png");
+
+
 
 
         Image icon = new Image("Algoviz_Logo.png");
         stage.getIcons().add(icon);
         stage.setTitle("Algo Viz");
 
-
-//
-        ImageView logo = new ImageView(img);
+        stage.setScene(scene);
         stage.show();
     }
 
